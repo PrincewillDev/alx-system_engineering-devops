e718ba10a0660f9693fe70a62e32d00eb14edd07
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-03-12 14:37:17 +0100

    Feat: add Regex command to parse a log file

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(", ")
+
+puts ARGV[0].scan(/\[from:([\+?0-9A-Za-z]+)\] \[to:(\+?\d+)\] \[flags:(-?\d+:-?\d+:-?\d+:-?\d+:-?\d+)\]/).join(",")
