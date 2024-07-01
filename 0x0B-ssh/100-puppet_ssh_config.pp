f6e5cd500924bb0afbb13141bd1bf6a81a89d87e
PrincewillDev <princewillfidelis1@gmail.com>
2024-07-01 02:59:03 +0100

    Setup a puppet file to configure ssh on my local device to access a server without password

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
-file_line{'Turn off password auth'
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no'
+# Set up client SSH configuration file so user can connect without password
+file_line {'Turn off passwd auth':
+	ensure => 'present',
+	path => '/etc/ssh/ssh_config',
+	line => '	PasswordAuthentication no',
 }
 
-file_line{'Ensure identity file'
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school'
+file_line {'Declare identity file':
+	ensure => 'present',
+	path => '/etc/ssh/ssh_config',
+	line => '	IdentityFile ~/.ssh/school',
 }
