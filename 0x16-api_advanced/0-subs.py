e795405c799f87b81680c69c1027c1915c4ccb16
PrincewillDev <princewillfidelis1@gmail.com>
2024-08-14 02:32:05 +0100

    Completed task: {queried reddit api to return number of subscribers for a given subreddit}

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a function that queries the Reddit
+API and returns the number of subscribers for a given subreddit.
+"""
+import requests
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    header = {"User-Agent": "my-reddit-app/0.1 by Princewill_Fidelis"}
+    response = requests.get(url, headers=header, allow_redirects= False)
+
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    else:
+        return 0
